aaf472b9643d36fdc746caab2e2fc1c3b89ab7b5
Haruka <96925398+nakoyasha@users.noreply.github.com>
2026-02-17 19:33:59 +0200

    feat: imagelabel scaling

--- engine/instances/ui/ImageLabel.cpp
+++ engine/instances/ui/ImageLabel.cpp
@@ -1,4 +1,7 @@
 #include "ImageLabel.h"
+
+#include <cmath>
+
 #include "raylib.h"
 
 using namespace Nyanners::Instances;
@@ -26,12 +29,35 @@ int ImageLabel::luaNewIndex(lua_State* context, std::string keyName, std::string
     }
 }
 
+
+
 void ImageLabel::draw()
 {
     if (!m_visible) return;
-    Rectangle source = {0.0f, 0.0f, (float)texture.width, (float)texture.height};
-
     if (!IsTextureValid(texture)) return;
+    SetTextureFilter(texture, TEXTURE_FILTER_BILINEAR);
+    const float aspectRatioDest = renderingRectangle.width / renderingRectangle.height;
+    const float aspectRatioTexture = texture.width / texture.height;
+
+
+    Rectangle source = {0.0f, 0.0f, static_cast<float>(texture.width), static_cast<float>(texture.height)};
+
+    if (aspectRatioTexture > aspectRatioDest) {
+        const float scaledWidth = texture.height * aspectRatioDest;
+        const float crop = (texture.width - scaledWidth) * 0.5f;
+
+        float left = std::clamp(crop, 0.0f, static_cast<float>(texture.width));
+        float right = std::clamp(crop + scaledWidth, 0.0f, static_cast<float>(texture.width));
+
+        source.x = crop;
+        source.width = scaledWidth;
+    } else {
+        const float scaledHeight = texture.width / aspectRatioDest;
+        const float crop = (texture.height - scaledHeight) * 0.5f;
+
+        source.y = crop;
+        source.height = scaledHeight;
+    }
 
     DrawTexturePro(texture, source, renderingRectangle, Vector2(), 0.0f, WHITE);
 }
