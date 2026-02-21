3584d1069b47d25ee48467266b47870673d19e4c
Haruka <96925398+nakoyasha@users.noreply.github.com>
2026-02-21 03:27:29 +0200

    convert the engine into a static library and create a separate executable linked with it

--- app/main.cpp
+++ app/main.cpp
@@ -5,13 +5,9 @@ void engine_main()
     Application::instance().start();
 }
 
-// void headless_main()
-// {
-    // Application::instance(true).start();
-// }
-
 int main(int argc, char** argv)
 {
+    Nyanners::Logger::log("Let's initialize the engine, despite us being in the app!");
     engine_main();
     return 0;
 }
