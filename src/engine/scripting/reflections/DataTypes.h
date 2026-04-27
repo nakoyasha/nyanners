b34f00f19eac7c3029b17bd1f025fc463bc837ad
Haruka <96925398+nakoyasha@users.noreply.github.com>
2026-04-27 17:47:49 +0300

    bad idea

--- src/engine/scripting/reflections/DataTypes.h
+++ src/engine/scripting/reflections/DataTypes.h
@@ -3,6 +3,6 @@
 #include "instances/services/ReflectionService.h"
 
 namespace Nyanners::Scripting::Reflection {
-	int push_color3(lua_State* context, DataTypes::Color3& color);
+	int push_color3(lua_State *context, DataTypes::Color3 &color);
 	int push_vector3 (lua_State* context, glm::vec3& color);
 }
