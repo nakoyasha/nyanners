eecc35634af03d5a59537c2db42b5b9d26b86fb4
Haruka <96925398+nakoyasha@users.noreply.github.com>
2026-05-29 19:23:05 +0300

    add support for flipping the texture in the fragment shader

--- assets/shaders/frag.glsl
+++ assets/shaders/frag.glsl
@@ -7,13 +7,20 @@ in vec2 vTexCoord;
 uniform vec4 uColor;
 uniform sampler2D uTexture;
 uniform bool uTextureSet;
+uniform bool uFlipTexture;
 
 void main() {
+    vec2 texCoord = vTexCoord;
+
+    if (uFlipTexture) {
+        texCoord = vec2(texCoord.x, 1.0 - vTexCoord.y);
+    }
+
     if (uTextureSet) {
-        vec4 texColor = texture(uTexture, vTexCoord);
+        vec4 texColor = texture(uTexture, texCoord);
         color = uColor * texColor;
     } else {
         color = uColor;
     }
-    //color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    // color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
